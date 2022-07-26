93653b53e84567636a4e01c1051b0f31ecc13808
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-25 23:02:11 -0300

    teste

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -2,11 +2,11 @@ package com.dio;
 
 public class PrimeiroPrograma {
 //EmoTe! 79/.CaDeRnO
-	public static void main(String[] args) {			
+	public static void main(String[] args) {		
 		int a = 2;
 		int b = 3;
 		System.out.println("Hello World! " + (a + b));
-	}
+ }
 }
 
  
