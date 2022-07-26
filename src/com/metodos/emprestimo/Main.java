d85856affdc610bccafa33db29e74cdb3b7c995d
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-26 15:26:54 -0300

    intro met_ exercico emprestimo

---
+++ src/com/metodos/emprestimo/Main.java
@@ -0,0 +1,9 @@
+package com.metodos.emprestimo;
+
+public class Main {
+        public static void main(String[] args){
+            Emprestimo.calcular(1000, 2);
+            Emprestimo.calcular(10000, 3);
+
+        }
+}
