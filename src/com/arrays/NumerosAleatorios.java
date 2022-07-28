6ce452e132c9be68ae07be8928fea1cd0f34f6ee
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-27 22:48:15 -0300

    Array, intro ao Random

---
+++ src/com/arrays/NumerosAleatorios.java
@@ -0,0 +1,25 @@
+package com.arrays;
+import java.util.Random;
+
+public class NumerosAleatorios {
+    public static void main(String[] args){
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0 ; i < numerosAleatorios.length ; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.println("Numeros Aleatórios: ");
+        for(int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+
+        System.out.println("Sucessores dos números aleatórios: ");
+        for(int numero : numerosAleatorios ) {
+            System.out.println((numero+1) + " ");
+        }
+    }
+}
