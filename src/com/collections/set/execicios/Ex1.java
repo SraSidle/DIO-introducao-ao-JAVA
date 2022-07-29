061a39ab9232c99ae977c01c9d32f8ddfaf8d420
Bianca Aguiar <biancaaguiar2021@gmail.com>
2022-07-28 23:14:17 -0300

    aprofundamento e exercicio set col

---
+++ src/com/collections/set/execicios/Ex1.java
@@ -0,0 +1,19 @@
+package com.collections.set.execicios;
+
+public class Ex1 {
+    /*Crie um conjunto contendo todas as cores do arcoiris
+     * Exiba todas as cores uma abaixo da outras
+     * A quantidade de cores que o arco iris tem
+     * Em ordem alfabética(Natural)
+     * Exiba as cores na ordem inversa da qual foi formada
+     * Exiba as que começam com v e a que não começam
+     * Limpe o conjunto e confira se está vazio
+     */
+
+     /* Crie uma classe linguagemFavorita que possua os atributos nome, ano de criacao e ide 
+      * Em seguida crie um conjunto com 3 linguagens e ordene-as.
+
+      * Por ordem de inserção, ordem natural, ide, ano de criacao e nome,  ano, nome e ide; 
+      * Ao final, exiba as ling no console, uyma abaixo da outra
+      */
+}
